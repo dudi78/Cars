1a66763065ea5263e9ad3b2081f2fb3ecc7d4553
dunia <hinddouniati@gmail.com>
2024-05-10 14:55:22 +0100

    update and delete_annonce_successfully

---
+++ src/main/java/com/example/sells/dao/entities/Reclamation.java
@@ -0,0 +1,12 @@
+package com.example.sells.dao.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+@Entity
+public class Reclamation {
+    @Id
+    private Integer id;
+
+}
